9e72c061eadba02827e146839100676235ee8130
S_George <ssemagandageorge480@gmail.com>
2023-06-23 08:54:56 +0000

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,57 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(TestApp());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class TestApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+        home: Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+        backgroundColor: Colors.red,
+        title: Text(
+          'Welcome to the JojApp',
         ),
       ),
-    );
+      body: Center(
+        child: Text("Hello User"),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   backgroundColor: Colors.red,
+      //   child: Icon(Icons.add),
+      //  ),
+    ));
   }
 }
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(Joj());
+// }
+
+// class Joj extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         home: Scaffold(
+//             appBar: AppBar(
+//               title: Text('Welcome to Joj.Inc'),
+//               backgroundColor: Colors.purple,
+//             ),
+//             body: Center(
+//               child: Text(
+//                 'Hello User, AVOID USING CHATGPT.',
+//                 textScaleFactor: 1.5,
+//                 style: TextStyle(
+//                   fontWeight: FontWeight.bold,
+//                   color: Colors.blue,
+
+//                   // backgroundColor: Color(0xff968998),
+//                 ),
+//               ),
+//             )));
+//   }
+// }
